05f6d75bcb8135d87606eef742f6ef52e2539702
Lucas Aversa Martins <d857486@rede.sp>
2026-06-11 11:04:25 -0300

    Ajustes no usuário do bookstack e ajuste na quantidade de tentativas de healthcheck do bookstack

--- mariadb/init/01-create-dbs.sql
+++ mariadb/init/01-create-dbs.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXISTS bookstack2 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS bookstack CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE USER IF NOT EXISTS 'bookstack_user'@'%' IDENTIFIED BY 'bookstack8432';
 
-GRANT ALL PRIVILEGES ON bookstack2.* TO 'bookstack_user'@'%';
+GRANT ALL PRIVILEGES ON bookstack.* TO 'bookstack_user'@'%';
 
 FLUSH PRIVILEGES;
